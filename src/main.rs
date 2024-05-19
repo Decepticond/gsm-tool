db65fb67845abf9aba2d385c072b51f95b251e9e
Lazaurus <lucillespinkamazon@gmail.com>
2024-05-18 23:42:56 -0400

    Xor implementation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 /*
+
 Aleksandr S 
-Some time in May
+XOR CL Tool
 
 */
 
 use std::io;
 
 fn main() {
+    println!("Enter two binary values to XOR, seperate each by a space, ex: ???? 10101010 10100101");
+
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            println!("{} bytes read", n);
-            println!("{}", input);
+        Ok(_) => {
+            let inputs: Vec<&str> = input.trim().split_whitespace().collect();
+            if inputs.len() == 2 {
+                let input1 = inputs[0];
+                let input2 = inputs[1];
+
+                let int1 = u64::from_str_radix(input1, 2).unwrap();
+                let int2 = u64::from_str_radix(input2, 2).unwrap();
+
+                let result  = int1^int2;
+                let b_result = format!("{:b}", result);
+                println!("{}", b_result);
+            } else {
+                println!("Incorrect syntax.");
+            }
         }
-        Err(error) => println!("error: {error}"),
+        Err(error) => println!("error: {}", error),
     }
 }
