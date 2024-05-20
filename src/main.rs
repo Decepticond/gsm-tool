7d7d9f33ed259f8a8c3ce6524ff3a499ad8a060a
Lazaurus <lucillespinkamazon@gmail.com>
2024-05-20 00:02:05 -0400

    Added boilerplate for to-be added cmds

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,36 @@ Aleksandr S
 XOR CL Tool
 
 */
+
+use clap::Parser;
 mod xor_tool;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cmd {
+    #[command(subcommand)]
+    subcmd: Option<SubCommand>,
+}
+
+#[derive(Parser, Debug)]
+enum SubCommand {
+    #[command(about = "XOR sub-command. Takes 2 binary values, XORs them, and outputs the result.")]
+    Xortool,
+
+    #[command(about = "Outputs a list of commands ")]
+    Cmdlist,
+}
+
 fn main() {
-    xor_tool::xor_tool();
+    let args = Cmd::parse();
+
+match args.subcmd {
+    Some(subcmd) => match subcmd {
+        SubCommand::Xortool => xor_tool::xor_tool(),
+        SubCommand::Cmdlist => println!("Test output"),
+    },
+    None => {
+        println!("No targets given."); 
+    }
+}
 }
