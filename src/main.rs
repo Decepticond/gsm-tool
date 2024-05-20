7dd031fcb8054f933a0ec3e02e5d1ec25b67d5a9
Lazaurus <lucillespinkamazon@gmail.com>
2024-05-20 00:17:18 -0400

    Funny

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,50 @@ enum SubCommand {
 
     #[command(about = "Outputs a list of commands ")]
     Cmdlist,
+
+    //#[command(about = "unknown ")] -- don't uncomment until I figure out what to make next
+    //Untool, 
+
+    //#[command(about = "unknown ")]
+    //Untool2,
 }
 
 fn main() {
+    println!("    ...           ..       ...                    ...              .. 
+    ..              .     ...    ......         ...                .. 
+   ..                 .    ..          ...     ..                  .. 
+   ..                  ..   ..            ... ..                   .. 
+   ..                   ... ...             ....                   .. 
+   .                     ......               ..                   .. 
+   .                   ..........                                  .. 
+                      ............                                 .. 
+   .                                                               .. 
+   ..                                                              .. 
+   ..                                                              .. 
+    ..                                                            ..  
+     ..                                  .....''........         ..   
+      ..     ........''''''.             .,,,,,,'.   ..         ..    
+      ...    ...    .',,,,,..            .,,,,,,..    ..      ...     
+..........   ..     .',,,,'.            ..,,,,,,.     ..      ..  ..  
+... ....     ..     .',,,,'.             .',,,,'.      ..         ..  
+  ..         ..      .',,,'.              .',''.       .         .    
+   ...       ..       .....    ..           ..                ..      
+     ...                                           .....     ...      
+      .    ...                                      ..         ..     
+     .                         ...........                      ..    
+    ..                     .....                                 ..   
+   ...........                                        .......         
+              ......                                                  
+                 .......                        ..                    
+                   ..                            .                    
+                     ..                                               
+                      ..                                              
+                    ...                                               
+                    .....                            .                
+                       ..                            .                
+                      ..                              .               
+                      ..                               .              
+                      .                                .    \nWelcome to a GSM cmdline tool. Run gsm-tool cmdlist for a list of all subcommands and their descriptors.");
     let args = Cmd::parse();
 
     match args.subcmd {
